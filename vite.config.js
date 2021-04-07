0c88fc1f68056efffbb85b94688ee4afcedb3e0a
林岑影 <lincenying@qq.com>
2021-04-07 22:57:27 +0800

    pwa配置 on 2021-04-07 22:57:27

--- vite.config.js
+++ vite.config.js
@@ -80,21 +80,49 @@ export default () => {
                 base: '/',
                 manifest: {
                     name: 'M.M.F小屋',
-                    themeColor: '#54d9e0',
-                    msTileColor: '#000000',
-                    appleMobileWebAppCapable: 'yes',
-                    appleMobileWebAppStatusBarStyle: 'black',
-                    manifestPath: '/static/manifest.json',
-                    manifestOptions: {
-                        start_url: '/'
-                    },
-                    iconPaths: {
-                        favicon32: '/static/img/icons/favicon-32x32.png',
-                        favicon16: '/static/img/icons/favicon-16x16.png',
-                        appleTouchIcon: '/static/img/icons/apple-touch-icon-152x152.png',
-                        maskIcon: '/static/img/icons/safari-pinned-tab.svg',
-                        msTileImage: '/static/img/icons/msapplication-icon-144x144.png'
-                    }
+                    short_name: 'M.M.F小屋',
+                    theme_color: '#54d9e0',
+                    background_color: '#ffffff',
+                    icons: [
+                        {
+                            src: '/static/img/icons/android-chrome-48x48.png',
+                            sizes: '48x48',
+                            type: 'image/png'
+                        },
+                        {
+                            src: '/static/img/icons/android-chrome-72x72.png',
+                            sizes: '72x72',
+                            type: 'image/png'
+                        },
+                        {
+                            src: '/static/img/icons/android-chrome-96x96.png',
+                            sizes: '96x96',
+                            type: 'image/png'
+                        },
+                        {
+                            src: '/static/img/icons/msapplication-icon-144x144.png',
+                            sizes: '144x144',
+                            type: 'image/png'
+                        },
+                        {
+                            src: '/static/img/icons/android-chrome-168x168.png',
+                            sizes: '168x168',
+                            type: 'image/png'
+                        },
+                        {
+                            src: '/static/img/icons/android-chrome-192x192.png',
+                            sizes: '192x192',
+                            type: 'image/png'
+                        },
+                        {
+                            src: '/static/img/icons/android-chrome-512x512.png',
+                            sizes: '512x512',
+                            type: 'image/png'
+                        }
+                    ],
+                    start_url: '/',
+                    display: 'standalone',
+                    lang: 'zh-CN'
                 },
                 workbox: {
                     // workbox options for generateSW
