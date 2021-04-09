4dafd2bd5036efc3f04e3084d7401bb23b949080
林岑影 <lincenying@qq.com>
2021-04-09 13:49:44 +0800

    on 2021-04-09 13:49:44

--- src/mixins/global.js
+++ src/mixins/global.js
@@ -16,9 +16,9 @@ export default () => {
     // eslint-disable-next-line no-unused-vars
     const store = useStore()
 
-    // autoUnlock === true 不管fn返回什么, 都自动解锁
-    // autoUnlock === false 不管fn返回什么, 都不自动解锁
-    // autoUnlock === 'auto' 当fn返回true时, 不自动解锁, 返回其他值时, 自动解锁
+    // autoUnlock === true 不管 fn 返回什么, 都自动解锁
+    // autoUnlock === false 不管 fn 返回什么, 都不自动解锁
+    // autoUnlock === 'auto' 当 fn 返回 false 时, 不自动解锁, 返回其他值时, 自动解锁
     const useLockFn = (fn = () => {}, autoUnlock = 'auto') => {
         const [lock, toggleLock] = useToggle(false)
         return async (...args) => {
@@ -26,7 +26,7 @@ export default () => {
             toggleLock(true)
             try {
                 const $return = await fn(...args)
-                if (autoUnlock === true || (autoUnlock === 'auto' && $return !== true)) toggleLock(false)
+                if (autoUnlock === true || (autoUnlock === 'auto' && $return !== false)) toggleLock(false)
             } catch (e) {
                 toggleLock(false)
                 throw e
