d6aa285bedb405339dae44bd2ef6728aabce6026
林岑影 <lincenying@qq.com>
2023-12-07 15:05:38 +0800

    chore: 同步ssr版本

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 import { readFile } from 'node:fs/promises'
 import lincy from '@lincy/eslint-config'
-import plugin from '@unocss/eslint-plugin'
 
 const autoImport = JSON.parse(
     await readFile(new URL('./.eslintrc-auto-import.json', import.meta.url)),
@@ -8,20 +7,19 @@ const autoImport = JSON.parse(
 
 const config = lincy(
     {
+        unocss: true,
+        formatters: {
+            css: false,
+            graphql: true,
+            html: true,
+            markdown: true,
+            toml: false,
+        },
         overrides: {
             ignores: [
                 '**/assets',
                 '**/static',
             ],
-        },
-    },
-    {
-        plugins: {
-            '@unocss': plugin,
-        },
-        rules: {
-            ...plugin.configs.recommended.rules,
-            '@unocss/order': 'off',
         },
     },
     {
