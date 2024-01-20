1e06afe5caca11ab7eabc75dc27835b0f77fd52f
林岑影 <lincenying@qq.com>
2024-01-20 20:08:37 +0800

    feat: 同步ssr版本

--- src/entry-client.ts
+++ src/entry-client.ts
@@ -7,19 +7,28 @@ import Prism from 'prismjs'
 
 // highlight code
 import 'prismjs/components/prism-json'
+import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-typescript'
+import 'prismjs/components/prism-markup-templating'
+import 'prismjs/components/prism-php'
+import 'prismjs/components/prism-twig'
+import 'prismjs/components/prism-css'
+import 'prismjs/components/prism-sass'
+import 'prismjs/components/prism-less'
+import 'prismjs/components/prism-bash'
 
 import { createApp } from './main'
 
 import reloadPrompt from '@/components/reload-prompt.vue'
 
 import 'uno.css'
+import './assets/css/github-markdown.css'
+import './assets/css/hljs/googlecode.css'
 import '@kangc/v-md-editor/lib/style/base-editor.css'
-import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
+import './assets/css/vuepress.css'
 import 'vue-loading-overlay/dist/css/index.css'
 import 'element-plus/es/components/message/style/css'
 import 'element-plus/es/components/message-box/style/css'
-import './assets/css/github-markdown.css'
-import './assets/css/hljs/googlecode.css'
 import './assets/scss/style.scss'
 
 VueMarkdownEditor.use(vuePressTheme, {
